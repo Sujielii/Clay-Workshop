d96fc4294d547a5f3eea5d7068e2539a81c08671
Sujielii <lsujie@ocadu.ca>
2025-04-10 15:26:55 -0400

    Update tutorial-data.js

---
+++ tutorial-data.js
@@ -0,0 +1,157 @@
+const tutorialData = {
+    // 小狗教程
+    dog: {
+        title: "Time to Squish!",
+        rating: 3,
+        image: "图案/dog.png",
+        colors: ["FBF3E8", "F1E6D7", "D4AD96", "C89373", "5D4C4B"],
+        time: "15-25 minutes",
+        materials: "PVA glue， Rolling tool ",
+        steps: 0,
+        bgImage: "棕色狗过程/Squish-dog.png"
+    },
+    
+    // 青蛙教程
+    frog: {
+        title: "Time to Squish!",
+        rating: 3,
+        image: "图案/frog.png",
+        colors: ["DADFC4", "B3B898", "A8AE8A", "7C6761", "453731"],
+        time: "15-25 minutes",
+        materials: "PVA glue， Rolling tool",
+        steps: 1,
+        bgImage: "青蛙过程图片+背景+摁钮/Squish-frog.png"
+    },
+    
+    // 兔子教程
+    rabbit: {
+        title: "Time to Squish!",
+        rating: 4,
+        image: "图案/rabbit.png",
+        colors: ["F5EFE8", "F1DBCB", "F8CEBE", "9E2427", "30191F"],
+        time: "20-30 minutes",
+        materials: "Plastic knife，PVA glue，Tweezers，Rolling Tool",
+        steps: 2,
+        bgImage: "兔子过程/Squish-rabbit.png"
+    },
+
+    // 牛角包教程
+    bread: {
+        title: "Time to Squish!",
+        rating: 2,
+        image: "图案/bread.png",
+        colors: ["453731", "E3B780", "D39D5C", "5C3E2B", "1D150D"],
+        time: "10-20 minutes",
+        materials: "Plastic knife，PVA glue，Tweezers，Rolling Tool",
+        steps: 3,
+        bgImage: "牛角包过程图片+背景+摁钮/Squish-bread.png"
+    },
+
+    // 企鹅教程
+    penguin: {
+        title: "Time to Squish!",
+        rating: 4,
+        image: "图案/penguin.png",
+        colors: ["FAFAF2", "F5B384", "E47222", "342C28", "000000"],
+        time: "20-30 minutes",
+        materials: "Cutting mat，Clay pressing board，Plastic knife，PVA glue，Tweezers，Rolling tool",
+        steps: 4,
+        bgImage: "企鹅过程/Squish-penguin.png"
+    },
+
+    // 茄子教程
+    eggplant: {
+        title: "Time to Squish!",
+        rating: 2,
+        image: "图案/eggplant.png",
+        colors: ["C8C783", "B3B263", "C8A4C4", "AA70A3", "5B3F50"],
+        time: "10-20 minutes",
+        materials: "PVA glue，Rolling tool",
+        steps: 5,
+        bgImage: "茄子过程/Squish-eggplant.png"
+    },
+
+    // 小猪教程
+    pig: {
+        title: "Time to Squish!",
+        rating: 3,
+        image: "图案/pig.png",
+        colors: ["FBDCD9", "FBBBB8", "DFA8A5", "856561", "5A4A47"],
+        time: "15-25 minutes",
+        materials: "PVA glue，Rolling tool",
+        steps: 6,
+        bgImage: "小猪过程/Squish-pig.png"
+    },
+
+    // 雪人教程
+    snowman: {
+        title: "Time to Squish!",
+        rating: 5,
+        image: "图案/sonwman.png",
+        colors: ["FEF7EE", "F4A556", "A5B7A2", "849880", "8D715B"],
+        time: "30-40 minutes",
+        materials: "Cutting mat，Clay pressing board，Plastic knife，PVA glue，Tweezers，Rolling tool",
+        steps:7,
+        bgImage: "雪人过程/Squish-snowman.png"
+    },
+
+    // 蛋糕教程
+    cake: {
+        title: "Time to Squish!",
+        rating: 4,
+        image: "图案/cake.png",
+        colors: ["FFF9F0", "F4D9B4", "E0C6A0", "CA3842", "8D2027"],
+        time: "20-30 minutes",
+        materials: "Toothpick，Plastic knife，PVA glue，Round stick",
+        steps: 8,
+        bgImage: "蛋糕过程/Squish-cake.png"
+    },
+
+    // 狐狸教程
+    fox: {
+        title: "Time to Squish!",
+        rating: 3,
+        image: "图案/fox.png",
+        colors: ["FBF3E5", "F4A56A", "F18D45", "714331", "422B1E"],
+        time: "15-25 minutes",
+        materials: "PVA glue，Rolling tool",
+        steps: 9,
+        bgImage: "狐狸过程/Squish-fox.png"
+    },
+
+    // 黑白狗教程
+    bwdog: {
+        title: "Time to Squish!",
+        rating: 4,
+        image: "图案/b&w dog.png",
+        colors: ["FEFAEE", "F2EDDB", "E7E4DB", "D9D4D4", "181817"],
+        time: "20-30 minutes",
+        materials: "Cutting mat，Clay pressing board，Plastic knife，PVA glue，Tweezers，Rolling Tool",
+        steps: 10,
+        bgImage: "黑白狗过程/Squish-bwdog.png"
+    },
+
+    // 甜甜圈教程
+    donut: {
+        title: "Time to Squish!",
+        rating: 3,
+        image: "图案/donut.png",
+        colors: ["F9D496", "DBA789", "D39874", "F3A9B0", "EA6283"],
+        time: "15-25 minutes",
+        materials: "Cutting mat，Clay pressing board，Plastic knife，PVA glue，Tweezers",
+        steps: 11,
+        bgImage: "甜甜圈过程/Squish-donut.png"
+    },
+
+    // 树教程
+    tree: {
+        title: "Time to Squish!",
+        rating: 4,
+        image: "图案/tree.png",
+        colors: ["E1C27B", "BA9461", "A23231", "8B976B", "7E8D5D"],
+        time: "20-30 minutes",
+        materials: "Cutting mat，Clay pressing board，Plastic knife，PVA glue，Tweezers，Rolling tool",
+        steps: 12,
+        bgImage: "圣诞树过程/Squish-tree.png"
+    }
+}; 
